a0e3c1e542c2ba1453c67f07e9b4bf33ea43e0e5
LucasAll-dev <jl012593@gmail.com>
2025-04-23 17:49:13 -0300

    criacao dos cards de servico - Lucas-all

--- src/components/servico/Servicos.jsx
+++ src/components/servico/Servicos.jsx
@@ -3,7 +3,10 @@ import "./styles.css"
 const servicos = [
     {id: 1, link:"#", card_props: "card-props" , card_img: "card-img" , img: "../src/assets/img/logo.PNG", alt: "fotos dos servicos", titulo: "Pacote de Casamento", preco: "1200,0"},
     {id: 2, link:"#", card_props: "card-props" , card_img: "card-img" , img: "../src/assets/img/logo.PNG", alt: "fotos dos servicos", titulo: "Pacote de Casamento", preco: "1200,0"},
-    {id: 3, link:"#", card_props: "card-props" , card_img: "card-img" , img: "../src/assets/img/logo.PNG", alt: "fotos dos servicos", titulo: "Pacote de Casamento", preco: "1200,0"}
+    {id: 3, link:"#", card_props: "card-props" , card_img: "card-img" , img: "../src/assets/img/logo.PNG", alt: "fotos dos servicos", titulo: "Pacote de Casamento", preco: "1200,0"},
+    {id: 4, link:"#", card_props: "card-props" , card_img: "card-img" , img: "../src/assets/img/logo.PNG", alt: "fotos dos servicos", titulo: "Pacote de Casamento", preco: "1200,0"},
+    {id: 5, link:"#", card_props: "card-props" , card_img: "card-img" , img: "../src/assets/img/logo.PNG", alt: "fotos dos servicos", titulo: "Pacote de Casamento", preco: "1200,0"},
+    {id: 6, link:"#", card_props: "card-props" , card_img: "card-img" , img: "../src/assets/img/logo.PNG", alt: "fotos dos servicos", titulo: "Pacote de Casamento", preco: "1200,0"}
 ]
 
 function Servicos () {
@@ -13,12 +16,12 @@ function Servicos () {
             <div id="linha-servicos"></div>
             <section className="servicos-layout">
                 {servicos.map((servicos) => (
-                    <a key={servicos.id} className="card-props" href={servicos.link}>
-                        
+                    <a key={servicos.id} className="card-link"  href={servicos.link}>
+                        <div className="card-props">
                             <img className={servicos.card_img} src={servicos.img} alt={servicos.alt} />
                             <h3 className="titulo-servico">{servicos.titulo}</h3>
-                            <p>R${servicos.preco}</p>
-                        
+                            <p className="preco-card">R${servicos.preco}</p>
+                        </div>
                     </a>
                 ))}
             </section>
